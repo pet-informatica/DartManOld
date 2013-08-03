395055aead710b0747d8595a9ba36e56564ab3d5
Rafael Farias Marinheiro <rfm3@imac01.local>
2013-08-03 11:43:32 -0300

    Adicionando desenho no canvas

--- web/dartman.dart
+++ web/dartman.dart
@@ -1,5 +1,27 @@
 import 'dart:html';
 
+CanvasElement backgroundCanvas;
+CanvasElement gameCanvas;
+ImageElement sprites;
+
 void main() {
-  window.alert("yeah");
+  backgroundCanvas = query("#background");
+  gameCanvas = query("#game");
+  drawBackground();
+  drawGame();
+}
+
+void drawBackground(){
+  var context = backgroundCanvas.context2D;
+  Rect rect = backgroundCanvas.client;
+  context.clearRect(0, 0, rect.width, rect.height);
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, rect.width, rect.height);
+}
+
+void drawGame(){
+  var context = gameCanvas.context2D;
+  Rect rect = gameCanvas.client;
+  context.fillStyle = "red";
+  context.fillRect(50, 50, 30, 30);
 }
